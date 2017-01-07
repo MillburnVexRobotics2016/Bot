b77545fcc1fa02da912bc3c46f4de3b99691e6de
kshah123 <installer@MTPS.MILLBURN.ORG>
2017-01-06 21:38:35 -0500

    updating autonomus

--- comp.c
+++ comp.c
@@ -42,19 +42,19 @@
 
 void pre_auton()
 {
-  // Set bStopTasksBetweenModes to false if you want to keep user created tasks
-  // running between Autonomous and Driver controlled modes. You will need to
-  // manage all user created tasks if set to false.
+	// Set bStopTasksBetweenModes to false if you want to keep user created tasks
+	// running between Autonomous and Driver controlled modes. You will need to
+	// manage all user created tasks if set to false.
 
-  bStopTasksBetweenModes = true;
+	bStopTasksBetweenModes = true;
 
 	// Set bDisplayCompetitionStatusOnLcd to false if you don't want the LCD
 	// used by the competition include file, for example, you might want
 	// to display your team name on the LCD in this function.
 	// bDisplayCompetitionStatusOnLcd = false;
 
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+	// All activities that occur before the competition starts
+	// Example: clearing encoders, setting servo positions, ...
 }
 
 /*---------------------------------------------------------------------------*/
@@ -72,31 +72,31 @@ void drive(int direction){
 	int speed = 100;
 	if(direction == 1){
 		//forwards
-		motor[FL] = -speed;
-		motor[FR] = speed;
+		motor[FL] = speed;
+		motor[FR] = -speed;
 		motor[BL] = speed;
 		motor[BR] = -speed;
-	}else if(direction == 2){
+		}else if(direction == 2){
 		//backwards
+		motor[FL] = -speed;
+		motor[FR] = speed;
+		motor[BL] = -speed;
+		motor[BR] = speed;
+		}else if(direction == 3){
+		//turn left
 		motor[FL] = speed;
 		motor[FR] = -speed;
 		motor[BL] = -speed;
 		motor[BR] = speed;
-	}else if(direction == 3){
-		//turn left
-	  motor[FL] = speed;
-		motor[FR] = speed;
-		motor[BL] = -speed;
-		motor[BR] = -speed;
-	}else if(direction == 4){
+		}else if(direction == 4){
 		//turn right
 		motor[FL] = -speed;
-		motor[FR] = -speed;
+		motor[FR] = speed;
 		motor[BL] = speed;
-		motor[BR] = speed;
-	}else{
+		motor[BR] = -speed;
+		}else{
 		//stop
-	  speed = 0;
+		speed = 0;
 		motor[FL] = speed;
 		motor[FR] = speed;
 		motor[BL] = speed;
@@ -105,10 +105,10 @@ void drive(int direction){
 }
 
 //void off {
-	//	motor[FL] = 0;
-	//	motor[FR] = 0;
-	//	motor[BL] = 0;
-	//	motor[BR] = 0;
+//	motor[FL] = 0;
+//	motor[FR] = 0;
+//	motor[BL] = 0;
+//	motor[BR] = 0;
 //}
 
 //float getDesiredClawPos(){
@@ -139,84 +139,63 @@ task autonomous()
 	slaveMotor(TLArms, BArms);
 	slaveMotor(TRArms, BArms);
 	drive(FORWARDS);
-	wait1Msec(1750);
+	wait1Msec(1700);
 	drive(STOP);
 
-	clawclose(100);
-	wait1Msec(300);
-	liftup(56);
+	clawopen(100);
+	wait1Msec(1000);
+	liftup(35);
 
 	drive(FORWARDS);
-	wait1Msec(300);
+	wait1Msec(100);
 	drive(STOP);
 
 
 	wait1Msec(200);
-	drive(TURNRIGHT);
-	wait1Msec(800);
+	drive(TURNLEFT); //turns left
+	wait1Msec(1360);
 	drive(STOP);
 
 	drive(BACKWARDS);
+	wait1Msec(750);
 	liftup(127);
-	wait1Msec(650);
+	wait1Msec(650); //lift first time
 	drive(STOP);
 
-	clawOpen(30);
+
+	clawclose(70);
 	wait1Msec(500);
-	clawOpen(0);
+	liftup(0);
+	wait1Msec(500);
+	clawopen(0);
 
+	liftdown(127);
+	wait1Msec(2000);
+	liftdown(0); //stop going up
 
-	while(SensorValue[armPo] <= 1700){
-		liftup(127);
-	}
-	if(SensorValue[armPo] >= 1700){
-		liftup(0);
-	}
-	if(SensorValue[armPo] > 1500){
-		motor[Claws] = 127;
-	}
-	if(SensorValue[armPo] > 1700){
-		motor[Claws] = 0;
-		motor[BArms] = 0;
-	}
-
-
-
-	wait1Msec(1000);
-
-	wait1Msec(200);
-	while(SensorValue[armPo] <= 1700 && SensorValue[armPo] > 0){
-		liftdown(127);
-	}
+	drive(TURNRIGHT);
+	wait1Msec(250);
+	drive(STOP);
 
 	drive(FORWARDS);
-	wait1Msec(500);
+	wait1Msec(1300);
 	drive(STOP);
 
-	clawclose(127);
-	wait1Msec(300);
+	clawopen(127);
+	wait1Msec(800);
 	liftup(56);
 
-	drive(TURNLEFT);
-	wait1Msec(200);
-	drive(STOP);
+	//
 
 	drive(BACKWARDS);
 	wait1Msec(1600);
 	drive(STOP);
 
-
-
-
-
-
-
-
-
-
-
+	clawclose(127);
+	wait1Msec(600);
+	clawclose(0);
+	liftdown(0);
 }
-
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -232,19 +211,61 @@ task autonomous()
 //called in the main task
 task driveM()
 {
+	int tval;
+	int fval;
 
 	int deadzone = 20;
 	while(true)
 	{
-		if(vexRT[Ch3] > deadzone || vexRT[Ch3] < -deadzone || vexRT[Ch1] > deadzone || vexRT[Ch1] < -deadzone)
-		{
-			motor[FL] = -vexRT[Ch3] - vexRT[Ch1];
-			motor[BL] = vexRT[Ch3] + vexRT[Ch1];
-			motor[FR] = vexRT[Ch3] - vexRT[Ch1];
-			motor[BR] = -vexRT[Ch3] + vexRT[Ch1];
+		tval = 0;
+		fval = 0;
+
+		if(
+			abs(vexRT[Ch1]) > deadzone || abs(vexRT[Ch4]) > deadzone){
+			if(abs(vexRT[Ch1]) > abs(vexRT[Ch4])){
+				fval = -vexRT[Ch1];
+			}
+			else
+			{
+				fval = -vexRT[Ch4];
+			}
+		}else{
+			fval = 0;
+	  }
+
+		if(abs(vexRT[Ch3]) > deadzone){
+			tval = vexRT[Ch3];
+		}else{
+			tval = 0;
 		}
-		else
-		{
+
+		if(fval != 0 || tval != 0){
+
+			int L = -fval + tval;
+
+			int R = -fval - tval;
+
+			L=-fval+tval;
+
+			R=-fval-tval;
+
+			if(L > 127)
+				L = 127;
+
+			if(L < -127)
+				L = -127;
+
+			if(R > 127)
+				R = 127;
+
+			if(R < -127)
+				R = -127;
+
+			motor[FL] = L;
+			motor[BL] = L;
+			motor[FR] = R;
+			motor[BR] = R;
+			}else{
 			motor[FL] = motor[BL] = motor[FR] = motor[BR] = 0;
 		}
 	}
@@ -261,6 +282,7 @@ task usercontrol()
 	//float desiredPos Btn7U
 	int triggered = 0;
 	int controls = 0;
+	//resetSensor(in1);
 	int armd1, armd2, armu1, armu2, clawo1, clawo2, clawc1, clawc2;
 	if(controls == 0){
 		armd1 = Btn5D;
@@ -271,7 +293,7 @@ task usercontrol()
 		clawo2 = Btn8U;
 		clawc1 = Btn6U;
 		clawc2 = Btn8R;
-	}else{
+		}else{
 		armd1 = Btn5U;
 		armd2 = Btn8D;
 		armu1 = Btn7L;
@@ -280,49 +302,45 @@ task usercontrol()
 		clawo2 = Btn8U;
 		clawc1 = Btn6U;
 		clawc2 = Btn8R;
- 	}
+	}
 	while(true)
 	{
 
 		//main arm up down
+
+		//arm notes
+		//a negative value sends arm up, positive down
 		//armPo is the shaft encoder for the arms, values go from 0 to about 4000, actually allows 265 degrees of movement for the axle
 		//up
 		if((vexRT[armd1] || vexRT[armd2]) && triggered == 0){
 			motor[BArms] = -127;
-		}else if((vexRT[armu1] || vexRT[armu2]) && triggered == 0){
-		//down
-			motor[BArms] = 100;
-		//}else if(triggered == 0 && SensorValue[armPo] > 150){
-		//	motor[BArms] = 20;
-		}else if(triggered == 0){
+			}else if((vexRT[armu1] || vexRT[armu2]) && triggered == 0){
+			//down
+			motor[BArms] = 127;
+			//}else if(triggered == 0 && SensorValue[armPo] > 150){
+			//	motor[BArms] = 20;
+			}else if(triggered == 0){
 			motor[BArms] = 0;
-	 	}
-
-		//if(desiredPos == getDesiredClawPos() > -0.5){
-		//	int curClawPos = 0;
-		//	if(desiredPos > curClawPos == SensorValue[clawPo]){
-
-		//	}
-	 // }else
-
+		}
 		//claw open and close
-	  if((vexRT[clawo1] || vexRT[clawo2])){
-			motor[Claws] = 80;
-		}else if(vexRT[clawc1] || vexRT[clawc2]){
-			motor[Claws] = -80;
-		}else{
+		if((vexRT[clawo1] || vexRT[clawo2])){
+			motor[Claws] = 127;
+			}else if(vexRT[clawc1] || vexRT[clawc2]){
+			motor[Claws] = -127;
+			}else{
 			motor[Claws] = 0;
 		}
 
 		//auto throw over the fence
-		if((vexRT[Btn7D] && SensorValue[armPo] < 1700) && 1 == 2|| triggered == 1)
+		int height = 3250;
+		if((vexRT[Btn7D] && SensorValue[armPo] < height - 700)|| triggered == 1)
 		{
 			triggered = 1;
 			motor[BArms] = 127;
-			if(SensorValue[armPo] > 1000){
+			if(SensorValue[armPo] > height - 700){
 				motor[Claws] = 127;
 			}
-			if(SensorValue[armPo] > 1700){
+			if(SensorValue[armPo] > height){
 				motor[Claws] = 0;
 				motor[BArms] = 0;
 				triggered = 0;
@@ -330,14 +348,14 @@ task usercontrol()
 		}
 
 		if(vexRT(Btn7U) == 1){
-				motor[Claws] = 0;
+			motor[Claws] = 0;
 			motor[BArms] = 0;
 			triggered = 0;
 			motor[FL] = 0;
 			motor[BL] = 0;
 			motor[FR] = 0;
 			motor[BR] = 0;
-	}
+		}
 
 		if(vexRT[Btn8DXmtr2]){
 			if(SensorValue[armPo] < 300){
